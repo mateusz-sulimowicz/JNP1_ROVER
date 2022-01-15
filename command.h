2fe8fc74628652ef0218b2b5f831d49bd083af19
mateusz <ms429603@students.mimuw.edu.pl>
2022-01-15 09:32:16 +0100

    Use unique_ptr in Command

--- command.h
+++ command.h
@@ -21,25 +21,25 @@ Command compose(std::initializer_list<Command> commands) {
 
 Command move_forward() {
     Command c;
-    c.push_back(std::move(std::make_unique<MoveForward>()));
+    c.push_back(std::make_unique<MoveForward>());
     return c;
 }
 
 Command move_backward() {
     Command c;
-    c.push_back(std::move(std::make_unique<MoveBack>()));
+    c.push_back(std::make_unique<MoveBack>());
     return c;
 }
 
 Command rotate_left() {
     Command c;
-    c.push_back(std::move(std::make_unique<RotateLeft>()));
+    c.push_back(std::make_unique<RotateLeft>());
     return c;
 }
 
 Command rotate_right() {
     Command c;
-    c.push_back(std::move(std::make_unique<RotateRight>()));
+    c.push_back(std::make_unique<RotateRight>());
     return c;
 }
 
